51a4c0551ba4174a7437340223e356180f5fc341
Yoiner Anderson Cabezas Arboleda <anderson.cabezas@mercurygate.com>
2023-01-26 13:09:43 -0500

    Unit testing

--- src/test/java/com/nisum/api/nisum/controller/UserControllerTest.java
+++ src/test/java/com/nisum/api/nisum/controller/UserControllerTest.java
@@ -1,21 +1,23 @@
 package com.nisum.api.nisum.controller;
 
-import com.nisum.api.nisum.business.BusinessInterface;
-import com.nisum.api.nisum.controller.mapper.ControllerMapper;
 import com.nisum.api.nisum.util.Constants;
 import com.nisum.api.nisum.util.DummyMock;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class UserControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -28,7 +30,29 @@ class UserControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.user.id").exists());
+    }
+
+    @Test
+    void shouldFailWhenInvalidEmailFormat() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                        .post("/userCreation")
+                        .content(DummyMock.asJsonString(DummyMock.getRequest(Constants.REQUEST_INVALID_MAIL_FILE_PATH)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.messages").value("must be a well-formed email address"));
+    }
+
+    @Test
+    void shouldFailWhenInvalidPasswordFormat() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                        .post("/userCreation")
+                        .content(DummyMock.asJsonString(DummyMock.getRequest(Constants.REQUEST_INVALID_PASSWORD_FILE_PATH)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.messages").value("password must contain at least one number"));
     }
 
 }
